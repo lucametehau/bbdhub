a3c406ba15d80c3964064b153960ede5025caf9a
lucametehau <lucametehau@gmail.com>
2024-11-28 08:51:28 +0100

    Add team members and a main.cpp.

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    if (argc != 5) {
+        std::cerr << "Expected 5 arguments!\nYour command should look like ./BBD -H <input history file> -m <output move file>.";
+        return 0;
+    }
+
+    // idk bro, wait
+    return 0;
+}
