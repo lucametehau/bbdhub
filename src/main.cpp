0f82e0a43e4cd8a4564a8858ce009e22ff0ab4dd
Emeric PAYER <emeric.payer@polytechnique.edu>
2024-11-28 18:19:46 +0000

    Squares

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "piece.h"
+#include "square.h"
+
 
 int main(int argc, char* argv[]) {
     // Decomment this when parser is implemented!
@@ -15,5 +17,10 @@ int main(int argc, char* argv[]) {
     std::cout << BBD::Pieces::BLACK_KNIGHT.to_char() << "\n";
     std::cout << BBD::Pieces::BLACK_QUEEN.to_char() << "\n";
     std::cout << BBD::Pieces::WHITE_KING.to_char() << "\n";
+
+    std::cout << BBD::Squares::A6 << "\n"; // 40
+    std::cout << BBD::Squares::B7 << "\n"; // 49
+    std::cout << BBD::Squares::C8 << "\n"; // 58
+
     return 0;
 }
