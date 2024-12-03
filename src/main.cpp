fecfcd8002b2943d3298e76bfeaa3db685afc3a9
Luca-Mihnea METEHAU <luca-mihnea.metehau@polytechnique.edu>
2024-12-03 21:14:19 +0000

    Introduce attacks

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,12 @@
 #include "piece.h"
 #include "square.h"
 #include "move.h"
+#include "attacks.h"
 
 using namespace BBD; // DON'T LEAVE THIS HERE
 
 int main(int argc, char* argv[]) {
+    BBD::attacks::init();
     // Decomment this when parser is implemented!
     /*
     if (argc != 1) {
@@ -14,11 +16,16 @@ int main(int argc, char* argv[]) {
     }
     */
 
-    using namespace BBD::Squares;
 
-    Move move(D8, E6, MoveTypes::NO_TYPE);
-    std::cout << move.to_string() << "\n"; // d8e6
-    move = Move(E2, E1, MoveTypes::PROMO_ROOK);
-    std::cout << move.to_string() << "\n"; // e2e1q
+    using namespace BBD::attacks;
+    using namespace BBD::Squares;
+    king_attacks[E2].print();
+    knight_attacks[E4].print();
+    Bitboard occ = (1ull << E3) | (1ull << D5);
+    generate_attacks<PieceTypes::BISHOP>(G2, occ).print();
+    generate_attacks<PieceTypes::BISHOP>(E4, occ).print();
+    generate_attacks<PieceTypes::ROOK>(E4, occ).print();
+    generate_attacks<PieceTypes::ROOK>(D1, occ).print();
+
     return 0;
 }
