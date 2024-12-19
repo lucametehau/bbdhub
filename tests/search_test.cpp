0bb4420a464e36da81fe0dad3bf7a2bd089f176b
rumata-feathers <georgii.kuznetsov@polytechnique.edu>
2024-12-19 12:09:52 +0100

    Bench:6472437 implement time_search

---
+++ tests/search_test.cpp
@@ -0,0 +1,52 @@
+#include <gtest/gtest.h>
+
+#include "test_utils.h"
+#include <iostream>
+
+using namespace BBD;
+using namespace BBD::Tests;
+using namespace BBD::Engine;
+
+class SearchTest : public ::testing::Test
+{
+  protected:
+    Board board;
+
+    void SetUp() override
+    {
+        board = Board();
+    }
+};
+
+TEST_F(SearchTest, TimeSearch1sec)
+{
+    BBD::attacks::init();
+    Board board("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8");
+    SearchLimiter limiter;
+    limiter.set_time(1000);
+    SearchThread thread;
+
+    print_board(board);
+    auto time_before = get_time_since_start();
+
+    thread.search(board, limiter);
+    auto t = (get_time_since_start() - time_before) / 1000.0; // in seconds
+    std::cerr << t;
+    EXPECT_LE(t, 1);
+}
+TEST_F(SearchTest, TimeSearch5sec)
+{
+    BBD::attacks::init();
+    Board board("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8");
+    SearchLimiter limiter;
+    limiter.set_time(5000);
+    SearchThread thread;
+
+    print_board(board);
+    auto time_before = get_time_since_start();
+
+    thread.search(board, limiter);
+    auto t = (get_time_since_start() - time_before) / 1000.0; // in seconds
+    std::cerr << t;
+    EXPECT_LE(t, 5);
+}
