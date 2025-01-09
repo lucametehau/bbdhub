e7535595b824ca40e7f6f2014814b8c12f3ab791
emeric.payer <emeric.payer@polytechnique.edu>
2025-01-09 14:59:14 +0300

    Implemented reverse futility pruning, bench 4486029

--- src/search.cpp
+++ src/search.cpp
@@ -9,7 +9,7 @@ void SearchThread::order_moves(MoveList &moves, int nr_moves)
 {
     std::array<int, 256> scores;
 
-    // try to rank captures higher
+    // Try to rank captures higher
     for (int i = 0; i < nr_moves; i++)
     {
         Move move = moves[i];
@@ -19,7 +19,7 @@ void SearchThread::order_moves(MoveList &moves, int nr_moves)
             scores[i] = history[board.player_color()][move.from()][move.to()];
     }
 
-    // simple sort
+    // Simple sort
     for (int i = 0; i < nr_moves; i++)
     {
         for (int j = i + 1; j < nr_moves; j++)
@@ -97,6 +97,7 @@ template <bool root_node> Score SearchThread::negamax(Score alpha, Score beta, i
     }
     if (depth == 0)
         return quiescence(alpha, beta);
+
     nodes++;
 
     if (limiter.get_mode() == SearchLimiter::SearchMode::TIME_SEARCH)
@@ -108,8 +109,19 @@ template <bool root_node> Score SearchThread::negamax(Score alpha, Score beta, i
         }
     }
 
-    // Principal variation search logic
+    // Reverse futility pruning
+    Score eval = board_evaluation(board) * (board.player_color() == Colors::WHITE ? 1 : -1);
 
+    if (!board.checkers() && depth <= 3)
+    {
+        int margin = 150 * depth; // could be smth else
+        if (eval >= beta + margin)
+        {
+            return eval;
+        }
+    }
+
+    // Principal variation search
     MoveList moves;
     int nr_moves = board.gen_legal_moves<ALL_MOVES>(moves);
 
@@ -162,47 +174,7 @@ template <bool root_node> Score SearchThread::negamax(Score alpha, Score beta, i
         }
     }
 
-    /*
-    // Alpha-Beta pruning logic
-
-    Score best = -INF;
-    int played = 0;
-
-    for (int i = 0; i < nr_moves; i++)
-    {
-        Move move = moves[i];
-        if (!board.is_legal(move))
-            continue;
-
-        board.make_move(move);
-        played++;
-
-        int score = -negamax<false>(-beta, -alpha, depth - 1, ply + 1);
-        board.undo_move(move);
-
-        if (score > best)
-        {
-            best = score;
-
-            if (score > alpha)
-            {
-                if constexpr (root_node)
-                {
-                    thread_best_move = move;
-                    thread_best_score = score;
-                }
-                alpha = score;
-                if (alpha >= beta)
-                {
-                    history[board.player_color()][move.from()][move.to()] += depth * depth;
-                    break;
-                }
-            }
-        }
-    }
-    */
-
-    // Now checking for checkmate / stalemate
+    // Check for checkmate / stalemate
     if (played == 0)
         return board.checkers() ? -INF + ply : 0;
 
@@ -215,7 +187,7 @@ Move SearchThread::search(Board &_board, SearchLimiter &_limiter)
     nodes = 0;
     board = _board, limiter = _limiter;
 
-    // fill history with 0 at the beginning
+    // Fill history with 0 at the beginning
     for (auto &t : history)
     {
         for (auto &p : t)
@@ -228,7 +200,7 @@ Move SearchThread::search(Board &_board, SearchLimiter &_limiter)
     int limit_depth = limiter.get_mode() == SearchLimiter::SearchMode::DEPTH_SEARCH ? limiter.get_depth() : 100;
 
     start_clock();
-    while (running && depth <= limit_depth) /// limit how much we can search
+    while (running && depth <= limit_depth) // limit how much we can search
     {
         try
         {
