5247b4b5799f8f1133e907f8bf581e1fe4d942e2
emeric.payer <emeric.payer@polytechnique.edu>
2025-01-24 15:51:49 +0100

    Changed some stuff in LMR. Bench: 497706

--- src/search.cpp
+++ src/search.cpp
@@ -238,16 +238,11 @@ template <bool root_node> Score SearchThread::negamax(Score alpha, Score beta, i
         Score score;
 
         // Late Move Reduction
-        bool do_lmr = (!root_node && !in_check && !board.is_capture(move) && depth >= 2 && played > 1);
+        bool do_lmr = (!root_node && !in_check && !board.is_capture(move) && depth >= 2 && played > 2);
 
         if (do_lmr)
         {
-            int reduction = 2;
-            if (depth >= 6)
-            {
-                reduction = 3;
-            }
-
+            int reduction = (depth >= 6) ? 3 : 2;
             score = -negamax<false>(-alpha - 1, -alpha, depth - 1 - reduction, ply + 1);
 
             if (score > alpha && score < beta)
@@ -257,20 +252,7 @@ template <bool root_node> Score SearchThread::negamax(Score alpha, Score beta, i
         }
         else
         {
-            if (played > 1)
-            {
-                // int reduction = 1;
-                score = -negamax<false>(-alpha - 1, -alpha, depth - 1 /*- reduction*/, ply + 1);
-
-                if (score > alpha && score < beta)
-                {
-                    score = -negamax<false>(-beta, -alpha, depth - 1, ply + 1);
-                }
-            }
-            else
-            {
-                score = -negamax<false>(-beta, -alpha, depth - 1, ply + 1);
-            }
+            score = -negamax<false>(-beta, -alpha, depth - 1, ply + 1);
         }
 
         /*
